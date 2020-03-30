5e9789e41b85b1ff962e40ae4ca13c39ab74c3ac
Shripad <sjoshi@splunk.com>
2020-03-30 18:37:50 +0530

    Removed dummy data

--- src/app.js
+++ src/app.js
@@ -4,24 +4,24 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './Routers/AppRouter';
 import configureStore from './store/configureStore';
-import {addExpense} from './actions/expenses';
-import getVisibleExpenses from './selectors/expenses';
+// import {addExpense} from './actions/expenses';
+// import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-    console.log(visibleExpenses);
-});
+// store.subscribe(() => {
+//     const state = store.getState();
+//     const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+//     console.log(visibleExpenses);
+// });
 
 
-store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
-store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
-store.dispatch(addExpense({description: 'Rent', amount: 1095000, createdAt: 1050}));
+// store.dispatch(addExpense({description: 'Water bill', amount: 4500}));
+// store.dispatch(addExpense({description: 'Gas bill', createdAt: 1000}));
+// store.dispatch(addExpense({description: 'Rent', amount: 1095000, createdAt: 1050}));
 
 // store.dispatch(setTextFilter('bill'));
 // store.dispatch(setTextFilter('water'));
