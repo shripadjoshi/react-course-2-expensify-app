e34feba5804790abe51cf717afd1e34f3b465614
Shripad <sjoshi@splunk.com>
2020-03-31 06:13:59 +0530

    added prod webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,36 @@
 
 const path = require('path');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, '/public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, '/public'),
+            filename: 'bundle.js'
         },
-        {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }
-    ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, '/public'),
-        historyApiFallback: true
-    } 
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+        },
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, '/public'),
+            historyApiFallback: true
+        } 
+    }
 };
