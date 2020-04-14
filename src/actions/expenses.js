eeae94eb917be95a17fdf4c9d6c4672830e45c63
Shripad <sjoshi@splunk.com>
2020-04-14 10:51:09 +0530

    added remove field

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,16 @@ export const removeExpense = ({ id } = {}) => ({
     id
 });
 
+//Remove Expense
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({ id }));
+        });
+
+    }
+};
+
 //EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
